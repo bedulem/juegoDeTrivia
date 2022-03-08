6558e152e131a7992577163042e1f8a85b23abfc
bedulem <santochibelen@gmail.com>
2022-03-08 19:13:39 -0300

    archivos base

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const lecturaYEscritura = require("./lecturaEscritura");
+// const preguntas = lecturaYEscritura.leerJson('preguntas');
+const preguntaP = document.getElementById("pregunta");
+
+
+
+// let generales = {
+//     preguntas,
+//     llamarPregunta: function(arr) {
+//         arr.forEach(pregunta => {
+//             // console.log(`id:${pregunta.id}, pregunta ${pregunta.desripcion}, "Fuente": " ${pregunta.fuente}`)
+//             preguntaP.innerHTML = `<p>${pregunta}</p>`
+
+//         });
+//     },
+//     BuscarPorID: function(id) {
+//         return this.pregunta.find(pregunta => pregunta.id === id)
+//     }
+// };
+
+// generales.llamarPregunta(preguntas);
+// // preguntaP.innerHTML = `<p>${generales.llamarPregunta(preguntas)}</p>`
+preguntaP.innerHTML = `laaaaaaaaaaaaaaaaaaaa`
